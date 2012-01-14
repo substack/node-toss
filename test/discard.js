863defd432bc637888654d246377bc99b7de5664
James Halliday <mail@substack.net>
2012-01-14 02:38:52 -0800

    busy timeout test passes

--- test/discard.js
+++ test/discard.js
@@ -15,6 +15,20 @@ test('errors', function (t) {
     }));
 });
 
+test('busy timeout', function (t) {
+    t.plan(1);
+    var tt = toss(15, function (err) {
+        t.equal(err.toString(), 'Error: timeout');
+        t.end();
+    });
+    
+    busy(tt(function () {
+        busy(tt(function () {
+            tt.end();
+        }))
+    }));
+});
+
 function busy (cb) {
     setTimeout(cb, 10);
 }
