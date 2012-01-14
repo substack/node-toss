f879dd9b44e02ac9d8ec2da3031dd9c7016c465b
James Halliday <mail@substack.net>
2012-01-13 19:07:03 -0800

    factored out http test

--- test/http.js
+++ test/http.js
@@ -1,35 +1,38 @@
 var test = require('tap').test;
-var toss = require('../');
 var http = require('http');
+var toss = require('../');
 
-test('http request timeout failure', function (t) {
+function withServer (cb) {
     var port = Math.floor(Math.random() * 5e5 + 1e4);
-    var tt = toss(200, function (err) {
-        t.ok(err);
-        t.end();
-    });
-    
-    function get (cb) {
+    var get = function (fn) {
         http.get({ port : port, path : '/' }, function (res) {
-            res.on('end', cb);
+            res.on('end', fn);
         });
-    }
-    
-    function ready () {
-        get(tt(function () {
-            get(tt(function () {
-                console.log('finished');
-                tt.end();
-            }));
-        }));
-    }
+    };
     
     var server = http.createServer(function (req, res) {
         setTimeout(function () {
             res.end('beeeeeeeeeeeeeeeeeeeeeeeeep');
         }, 600);
     });
-    server.listen(port, ready);
+    server.listen(port, cb.bind(null, get));
+    
+    return server;
+}
+
+test('http request timeout failure', function (t) {
+    var tt = toss(200, function (err) {
+        t.ok(err);
+        t.end();
+    });
+    
+    var server = withServer(function (get) {
+        get(tt(function () {
+            get(tt(function () {
+                tt.end();
+            }));
+        }));
+    });
     
     t.on('end', function () {
         server.close();
